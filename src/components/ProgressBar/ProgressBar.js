c76cb23611ed0157db9df6ba8f84f8bf15fc3548
Stephen Gibbon <steve64g@gmail.com>
2021-10-10 10:04:11 +1100

    updates to progress bar

--- src/components/ProgressBar/ProgressBar.js
+++ src/components/ProgressBar/ProgressBar.js
@@ -5,58 +5,69 @@ import styled from "styled-components";
 import { COLORS } from "../../constants";
 import VisuallyHidden from "../VisuallyHidden";
 
-const SIZES = {
+const STYLES = {
   large: {
-    height: "24px",
-    padding: "4px 4px",
-    radius: "8px",
+    height: 16,
+    padding: 4,
+    radius: 8,
   },
   medium: {
-    height: "12px",
-    padding: "",
-    radius: "4px",
+    height: 12,
+    padding: 0,
+    radius: 4,
   },
   small: {
-    height: "8px",
-    padding: "",
-    radius: "4px",
+    height: 8,
+    padding: 0,
+    radius: 4,
   },
 };
 
 const ProgressBar = ({ value, size }) => {
   value = Math.max(0, Math.min(100, value));
+  const style = STYLES[size] || STYLES.medium;
   return (
-    <Wrapper size={size} value={value}>
-      <div
-        role="progressbar"
-        aria-valuenow={value}
-        aria-valuemin="0"
-        aria-valuemax="100"
-        value={value}
-      ></div>
+    <Wrapper
+      size={size}
+      value={value}
+      role="progressbar"
+      aria-valuenow={value}
+      aria-valuemin="0"
+      aria-valuemax="100"
+      style={{
+        "--width": `${value}%`,
+        "--height": `${style.height}px`,
+        "--padding": `${style.padding}px`,
+        "--radius": `${style.radius}px`,
+      }}
+    >
+      <InnerWrapper>
+        <Inner>
+          <VisuallyHidden>{value}%</VisuallyHidden>
+        </Inner>
+      </InnerWrapper>
     </Wrapper>
   );
 };
 
 const Wrapper = styled.div`
-  width: 370px;
-  height: ${(p) => SIZES[p.size].height};
-  padding: ${(p) => SIZES[p.size].padding};
-  border-radius: ${(p) => SIZES[p.size].radius};
+  padding: var(--padding);
+  border-radius: var(--radius);
   background-color: ${COLORS.transparentGray15};
   box-shadow: inset 0px 2px 4px ${COLORS.transparentGray35};
+`;
 
-  & > div {
-    width: ${(p) => p.value + "%"};
-    border-top-left-radius: "4px";
-    border-bottom-left-radius: "4px";
-    border-top-right-radius: ${(p) =>
-      p.value < 98 ? 0 : p.value < 99 ? "2px" : "4px"};
-    border-bottom-right-radius: ${(p) =>
-      p.value < 98 ? 0 : p.value < 99 ? "2px" : "4px"};
-    background-color: ${COLORS.primary};
-    height: 100%;
-  }
+const InnerWrapper = styled.div`
+  border-radius: 4px;
+
+  /* trim inner bar at ends */
+  overflow: hidden;
+`;
+
+const Inner = styled.div`
+  height: var(--height);
+  width: var(--width);
+  background-color: ${COLORS.primary};
 `;
 
 export default ProgressBar;
