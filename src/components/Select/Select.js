380e459cd9ff0f766387eeda2f89ccb2d52ef746
Stephen Gibbon <steve64g@gmail.com>
2021-10-10 12:33:05 +1100

    did select - all but fix the arrow

--- src/components/Select/Select.js
+++ src/components/Select/Select.js
@@ -1,18 +1,39 @@
-import React from 'react';
-import styled from 'styled-components';
+import React from "react";
+import styled from "styled-components";
 
-import { COLORS } from '../../constants';
-import Icon from '../Icon';
-import { getDisplayedValue } from './Select.helpers';
+import { COLORS } from "../../constants";
+import Icon from "../Icon";
+import { getDisplayedValue } from "./Select.helpers";
 
 const Select = ({ label, value, onChange, children }) => {
   const displayedValue = getDisplayedValue(value, children);
 
   return (
-    <select value={value} onChange={onChange}>
+    <Wrapper value={value} onChange={onChange}>
       {children}
-    </select>
+    </Wrapper>
   );
 };
+
+const Wrapper = styled.select`
+  font-family: Roboto, sans-serif;
+  height: 43px;
+  padding: 12px 52px 12px 16px;
+  border-radius: 8px;
+  border: none;
+  background-color: ${COLORS.transparentGray15};
+  color: ${COLORS.gray700};
+
+  // the arrow
+  /* appearance: none; */
+
+  &:focus {
+    outline: solid ${COLORS.primary};
+  }
+
+  &:hover {
+    color: ${COLORS.black};
+  }
+`;
 
 export default Select;
